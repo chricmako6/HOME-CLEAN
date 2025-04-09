6348aee0735b381d55524e42bedd179acd8f526f
chricmako6 <156549010+chricmako6@users.noreply.github.com>
2025-04-09 23:37:23 +0300

    Hero section changes
    also there is a changes of shadow in index.css

--- src/Container/Hero.jsx
+++ src/Container/Hero.jsx
@@ -3,8 +3,12 @@ export default function Hero(){
 
     return(
         <>
-        <section className="container max-w-full align-center">
-         
+        <section className="shadow grid grid-cols-2  h-screen container max-w-full">
+         <div className="md:my-32 pl-20">
+            <h1 className="text-white">
+                Find Your<br/> <span className="text-Primary">Perfect</span> Cleaning<br/> Match!
+            </h1>
+         </div>
         </section>
         </>
     );
